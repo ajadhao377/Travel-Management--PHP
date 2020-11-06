9bd81a35f5d1074770b3406d02a1b842c0f64670
Ankush <ajadhao377@gmail.com>
2020-11-06 18:11:53 +0530

    Project By Ankush Jadhao

---
+++ includes/footer.php
@@ -0,0 +1,20 @@
+
+
+
+
+<!--- /footer-top ---->
+<!---copy-right ---->
+<div class="copy-right1" style="text-align:center;color:black;border:2px solid skyblue;background-color:skyblue">
+	<div class="container">
+	
+		<div class="footer-social-icons wow fadeInDown animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInDown;">
+			<ul >
+				<li><a  class="facebook" href="#"><span style="color:black">Facebook</span></a></li>
+				<li><a  class="twitter" href="#"><span style="color:black">Twitter</span></a></li>
+				<li><a  class="flickr" href="#"><span style="color:black">Flickr</span></a></li>
+				<li><a  class="googleplus" href="#"><span style="color:black">Google+</span></a></li>
+			</ul>
+		</div>
+		<p class="wow zoomIn animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;text-align:center;color:Black;font-weight:bold">© 2020 . All Rights Reserved &copy; </p>
+	</div>
+</div>
---
+++ index.php
@@ -0,0 +1,97 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Travel Management System</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<script type="applijewelleryion/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<link href='//fonts.googleapis.com/css?family=Open+Sans:400,700,600' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+<link href="css/font-awesome.css" rel="stylesheet">
+<!-- Custom Theme files -->
+<script src="js/jquery-1.12.0.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!--animate-->
+<link href="css/animate.css" rel="stylesheet" type="text/css" media="all">
+<script src="js/wow.min.js"></script>
+	<script>
+		 new WOW().init();
+	</script>
+<!--//end-animate-->
+</head>
+<body>
+<?php include('includes/header.php');?>
+<div>
+	<div class="container">
+		<h1 style="text-align:center;color:red;font-size:70px">Travel Management System</h1>
+	</div>
+</div>
+
+    
+<div class="container">
+	<div class="holiday">
+	
+
+
+
+	
+	<h3 style="text-align:center;color:purple;font-size:30px">Package List</h3>
+
+					
+<?php $sql = "SELECT * from tbltourpackages order by rand() limit 4";
+$query = $dbh->prepare($sql);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+$cnt=1;
+if($query->rowCount() > 0)
+{
+foreach($results as $result)
+{	?>
+			<div class="rom-btm">
+				<div class="col-md-3 room-left wow fadeInLeft animated" data-wow-delay=".5s">
+					<img src="admin/pacakgeimages/<?php echo htmlentities($result->PackageImage);?>" class="img-responsive" alt="">
+				</div>
+				<div class="col-md-6 room-midle wow fadeInUp animated" data-wow-delay=".5s">
+					<h4 style="color:#C50B2A;font-size:25px">Package Name: <?php echo htmlentities($result->PackageName);?></h4>
+					<h6 style="color:#D9B502;font-size:25px">Package Type : <?php echo htmlentities($result->PackageType);?></h6>
+					<p style="color:BLACK;font-size:15px"><b>Package Location :</b> <?php echo htmlentities($result->PackageLocation);?></p>
+					<p style="color:BLACK;font-size:15px"><b>Features</b> <?php echo htmlentities($result->PackageFetures);?></p>
+				</div>
+				<div class="col-md-3 room-right wow fadeInRight animated" data-wow-delay=".5s">
+					<h5>INR <?php echo htmlentities($result->PackagePrice);?></h5>
+					<a href="package-details.php?pkgid=<?php echo htmlentities($result->PackageId);?>" class="view">Details</a>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+
+<?php }} ?>
+			
+		
+<div><a href="package-list.php" class="view">View More Packages</a></div>
+</div>
+</div>
+
+
+
+
+<?php include('includes/footer.php');?>
+<!-- signup -->
+<?php include('includes/signup.php');?>			
+<!-- //signu -->
+<!-- signin -->
+<?php include('includes/signin.php');?>			
+<!-- //signin -->
+<!-- write us -->
+<?php include('includes/write-us.php');?>			
+<!-- //write us -->
+</body>
+</html>
